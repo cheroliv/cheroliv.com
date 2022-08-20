a31c6bdeb3d7785ab9e8ff34741b24c738960fd9
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-08-20 16:47:54 +0200

    add tests

--- codes/src/test/kotlin/playground/programming/RegularExpressionsTest.kt
+++ codes/src/test/kotlin/playground/programming/RegularExpressionsTest.kt
@@ -2,10 +2,20 @@
 
 package playground.programming
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class RegularExpressionsTest {
     @Test
     fun `expressions régulières`() {
+        val p: Pattern = Pattern.compile("honou?r")
+        val caesarUK = "For Brutus is an honourable man"
+        val mUK: Matcher = p.matcher(caesarUK)
+        assertEquals(true, mUK.find(), "Should matches UK spelling")
+        val caesarUS = "For Brutus is an honorable man"
+        val mUS: Matcher = p.matcher(caesarUS)
+        assertEquals(true, mUS.find(), "Should matches US spelling")
     }
 }
