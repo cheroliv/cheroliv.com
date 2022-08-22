08fb29bc3cc5e85df4bbf8e22da5315cdac98624
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-08-22 18:23:54 +0200

    update FilesAndDirectoriesTest.kt

--- codes/src/test/kotlin/playground/programming/FilesAndDirectoriesTest.kt
+++ codes/src/test/kotlin/playground/programming/FilesAndDirectoriesTest.kt
@@ -5,6 +5,8 @@ import kotlin.test.Test
 class FilesAndDirectoriesTest {
     @Test
     fun `fichiers et repertoires`(){
+        //voici certaines operations que vous pouvez effectuer avec java.io.File
+
 
     }
 }
