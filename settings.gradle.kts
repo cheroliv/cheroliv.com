faf4afb5131c6f09a46f1e183386d6ce78138646
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-12-31 19:19:56 +0100

    Clean, refactor, rename, update and rearrange.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,26 @@
 pluginManagement {
+
+    val jacksonVersion = "2.14.1"
+    val jgitVersion = "6.4.0.202211300538-r"
+    val commonsIoVersion = "2.11.0"
+    val slf4jVersion = "1.7.36"
+    val xzVersion = "1.9"
+
     buildscript {
         repositories {
-            mavenLocal()
             gradlePluginPortal()
             mavenCentral()
-            maven(url = "https://repo.eclipse.org/content/groups/releases/")
         }
         dependencies {
-            val jacksonVersion = "2.14.1"
-            val jgitVersion = "6.4.0.202211300538-r"
-            val commonsIoVersion = "2.11.0"
-            val slf4jVersion = "1.7.36"
-            val xzVersion = "1.9"
-
-            classpath(dependencyNotation = "org.slf4j:slf4j-simple:$slf4jVersion")
-            classpath(dependencyNotation = "commons-io:commons-io:$commonsIoVersion")
-
-            classpath(dependencyNotation = "com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-            classpath(dependencyNotation = "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
-            classpath(dependencyNotation = "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-
-            classpath(dependencyNotation = "org.eclipse.jgit:org.eclipse.jgit:$jgitVersion")
-            classpath(dependencyNotation = "org.eclipse.jgit:org.eclipse.jgit.archive:$jgitVersion")
-            classpath(dependencyNotation = "org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:$jgitVersion")
-            classpath(dependencyNotation = "org.tukaani:xz:$xzVersion")
+            classpath("org.slf4j:slf4j-simple:$slf4jVersion")
+            classpath("commons-io:commons-io:$commonsIoVersion")
+            classpath("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+            classpath( "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
+            classpath( "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+            classpath( "org.eclipse.jgit:org.eclipse.jgit:$jgitVersion")
+            classpath( "org.eclipse.jgit:org.eclipse.jgit.archive:$jgitVersion")
+            classpath( "org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:$jgitVersion")
+            classpath( "org.tukaani:xz:$xzVersion")
         }
     }
     plugins { id("org.jbake.site").version(extra["jbake_gradle_plugin_version"].toString()) }
@@ -31,10 +28,8 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        mavenLocal()
         google()
         mavenCentral()
-        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/") }
     }
 }
 rootProject.name = "cheroliv.com"
