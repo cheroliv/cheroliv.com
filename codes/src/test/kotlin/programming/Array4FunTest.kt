6e6454b8f4cbece507a0a92be7f623304f06252c
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-01-08 00:50:54 +0100

    Clean, refactor, rename, update and rearrange.

--- codes/src/test/kotlin/programming/Array4FunTest.kt
+++ codes/src/test/kotlin/programming/Array4FunTest.kt
@@ -1,6 +1,8 @@
 package programming
 
 import java.util.*
+import java.util.Arrays.setAll
+import java.util.Arrays.sort
 import kotlin.test.Test
 
 class Array4FunTest {
@@ -15,10 +17,10 @@ class Array4FunTest {
         // Obtain a number between [0 - 49].
         @Suppress("UNUSED_VARIABLE") val n = rand.nextInt(50)
         val intArr = arrayOfNulls<Int>(50)
-        Arrays.setAll(intArr) { rand.nextInt(50) }
+        setAll(intArr) { rand.nextInt(50) }
         println(intArr.contentToString())
         val intArrCopy = intArr.copyOf(intArr.size)
-        Arrays.sort(intArrCopy)
+        sort(intArrCopy)
         println(intArrCopy.contentToString())
     }
 }
