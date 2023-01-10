681c50935ab96b8365c24bf8a6df10511159a9ed
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-01-10 21:05:56 +0100

    Clean, refactor, rename, update and rearrange.

--- codes/src/main/kotlin/data/DataUtils.kt
+++ codes/src/main/kotlin/data/DataUtils.kt
@@ -5,15 +5,24 @@ import java.text.Normalizer.Form.NFD
 import java.text.Normalizer.normalize
 
 
+/**
+ *
+ */
 fun Array<String>.nameToLoginNormalizer(): Array<String> = map {
     it.lowercase().replace(' ', '.').unaccent()
 }.toTypedArray()
 
+/**
+ *
+ */
 @Suppress("unused")
 fun Array<String>.nameToLogin(): Array<String> = map {
     stripAccents(it.lowercase().replace(' ', '.'))
 }.toTypedArray()
 
+/**
+ *
+ */
 @Suppress("MemberVisibilityCanBePrivate", "SpellCheckingInspection")
 fun CharSequence.unaccent(): String = "\\p{InCombiningDiacriticalMarks}+"
     .toRegex()
